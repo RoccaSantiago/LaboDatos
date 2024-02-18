2857b994ffa90a0529fdfdda3d351f2f655af320
Santiago <Santiagorocca17@gmail.com>
2024-02-17 22:41:01 -0300

    xzzxzx

--- TP/Archivo.py
+++ TP/Archivo.py
@@ -10,8 +10,32 @@ Grupo: pip install grupo_1
 import pandas as pd
 
 
+
 #%%
 
 #Abrimos y guardamos los datasets:
 
-carpeta ="~/DataSets"
+carpeta ="C:/Users/Phone/Desktop/Source2/LaboDatos/TP/Datasets/original/"
+
+datos_banco_mundial = pd.read_csv(carpeta + "Datos_Banco_Mundial.csv")
+
+datos_basicos_sedes = pd.read_csv(carpeta + "Datos_Basicos_Sedes.csv")
+
+datos_completos_sedes = pd.read_csv(carpeta + "Datos_Completos_Sedes.csv")
+
+datos_Secciones_Sedes = pd.read_csv(carpeta + "Datos_Secciones_Sedes.csv")
+
+#%%
+
+#Creamos el dataframe vacio representando nuestro modelo relacional:
+    
+pais = pd.DataFrame(columns = ["iso3", "nombre", "pbi", "region"])
+
+sede = pd.DataFrame(columns = ["codigo", "nombre", "iso3"])
+
+seccion = pd.DataFrame(columns = ["descripcion", "titular_nombre", "titular_apellido", "codigo_sede"])
+
+red_social = pd.DataFrame(columns = ["link", "Nombre", "Sede"])
+
+#%%
+
